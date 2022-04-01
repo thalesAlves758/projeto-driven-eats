8cd0a37927d363ece5f689ef33a261076ca8c309
unknown <thalesalves758@gmail.com>
2022-03-31 23:10:02 -0300

    Adiciona uma função que verifica se o botao pode ser habilitado e outra que de fato habilita o botao

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,21 @@ function selectProduct(productEl, productTypeClass) {
     if(selectedElement) selectedElement.classList.remove("selected");
 
     productEl.classList.add("selected");
+
+    checkIfCanFinishRequest();
+}
+
+function checkIfCanFinishRequest() {
+    const plateSelected = document.querySelector('.plates .selected');
+    const drinkSelected = document.querySelector('.drinks .selected');
+    const dessertSelected = document.querySelector('.desserts .selected');
+
+    if(plateSelected && drinkSelected && dessertSelected) enableFinishRequestButton();
+}
+
+function enableFinishRequestButton() {
+    const finishRequestButton = document.querySelector('.bottom-bar button');
+
+    finishRequestButton.innerHTML = "Fechar pedido";
+    finishRequestButton.disabled = false;
 }
